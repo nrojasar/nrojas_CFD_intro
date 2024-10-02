2639b6c348be290690e2b5591ff2e3a28494ba47
[Student] Nicolas Rojas Aragon <nicolas.rojasaragon@IC-LIN-008.view.cranfield.ac.uk>
2024-10-02 17:04:54 +0100

    changes

--- tank_vis.m
+++ tank_vis.m
@@ -2,4 +2,41 @@ clear all;
 close all;
 clc;
 
-t = linspace(0,10,1001);
+%% parameters
+
+rho = 1000; % water density [kg/m^3]
+D = 1; % tank diameter [m]
+hO = 2; % initial water height [m]
+g = 9.81; % gravitational acceleration [m/s^2]
+c = 0.1; % valve coefficient [kg/s/Pa]
+
+%% Derived Quantities
+% Exponential time multiplier
+k = 4*c*g/D^2/pi; 
+
+% time to reach half-empty level
+
+t_half = (pi*D^2*log(2))/ (4*c*g);
+
+t_empty = (pi*D^2*log(1000))/ (4*c*g);
+
+h_avg = hO / (2*log(2));
+
+%% time
+
+t = linspace(0,t_empty,1001);
+
+%Solution function
+
+h = hO * exp (-k*t);
+
+%% Plotting
+
+figure(1)
+plot(t,h, 'r--', 'LineWidth', 2)
+
+
+
+
+
+
